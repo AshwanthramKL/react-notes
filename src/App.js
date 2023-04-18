e19970542e145cd4d89399d10021849d16208845
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-04-18 19:45:09 +0530

    JSX user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  const name = <h1>MARTIALEAGLE</h1>
+  const name = <h1>MARTIALEAGLE</h1>;
+  const age = <h2>21</h2>;
+  const email = <h2>k.l.ashwanthram@gmail.com</h2>;
+
+  const user = (
+    <div>
+      <div>
+        {name}
+        {age}
+        {email}
+      </div>
+    </div>
+  );
+
   return (
     <div className="App">
-      {name}
-      {name}
+      {user}
+      {user}
     </div>
   );
 }
