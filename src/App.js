3d999dfe1782d5c5b9fca110aaac8f8b7ef8c651
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-04-18 19:17:23 +0530

    JSX basics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
 function App() {
+  const name = <h1>MARTIALEAGLE</h1>
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      {name}
+      {name}
     </div>
   );
 }
