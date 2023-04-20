1c66f8c701ac8f8c61004d9c0a71c9faa5abf8ee
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-04-20 07:47:56 +0530

    condiitional rendering with &&

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 
-
 function App() {
   const age = 19;
-  
+  const isGreen = false;
+
   return (
     <div className="App">
-      {age > 18? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
+      {age > 18 ? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
+      <h1 style={{ color: isGreen ? "green" : "brown" }}> THIS IS A COLOR</h1>
+
+      {isGreen && <button>This is a button</button>}
     </div>
   );
 }
