1c27bc02ca9fb29fe785fd002d631d0e0bfad1a4
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-04-20 07:39:54 +0530

    conditional rendering with ternary operator

--- README.md
+++ README.md
@@ -143,3 +143,12 @@ function App() {
   );
 }
 ```
+
+
+## Conditional Rendering
+
+Uses the ternary operator.
+
+```javascript
+{age > 18? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
+```
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import styles from "./App.module.css";
+import "./App.css";
 
 
 function App() {
+  const age = 19;
+  
   return (
-    <div className={styles.App}>
-      <h1 className={styles.name}>MARTIALEAGLE</h1>
+    <div className="App">
+      {age > 18? <h1>OVER AGE</h1> : <h1>UNDER AGE</h1>}
     </div>
   );
 }
