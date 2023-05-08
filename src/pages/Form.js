05d9936e556646f64aeec4ae8f3335b8080e2036
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-05-08 20:28:05 +0530

    error handling in forms

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -4,17 +4,26 @@ import * as yup from "yup";
 
 export const Form = () => {
   const schema = yup.object().shape({
-    fullName: yup.string().required(),
-    email: yup.string().email().required(),
-    age: yup.number().positive().integer().min(18).required(),
-    password: yup.string().min(4).max(20).required(),
+    fullName: yup.string().required("Your full name is a required field"),
+    email: yup.string().email().required("This is a required field"),
+    age: yup
+      .number()
+      .positive()
+      .integer()
+      .min(18, "must be above 18yrs of age")
+      .required("This is a required field"),
+    password: yup.string().min(4).max(20).required("This is a required field"),
     confirmPassword: yup
       .string()
-      .oneOf([yup.ref("password"), null])
-      .required(),
+      .oneOf([yup.ref("password"), null], "Passwords don't match")
+      .required("This is a required field"),
   });
 
-  const { register, handleSubmit } = useForm({ resolver: yupResolver(schema) });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
 
   const onSubmit = (data) => {
     console.log(data);
@@ -23,22 +32,27 @@ export const Form = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type="text" placeholder="Full Name..." {...register("fullName")} />
+      <p>{errors.fullName?.message}</p>
       <br />
       <input type="text" placeholder="Email..." {...register("email")} />
+      <p>{errors.email?.message}</p>
       <br />
       <input type="number" placeholder="Age..." {...register("age")} />
+      <p>{errors.age?.message}</p>
       <br />
       <input
         type="password"
         placeholder="Password..."
         {...register("password")}
       />
+      <p>{errors.password?.message}</p>
       <br />
       <input
         type="password"
         placeholder="Confirm Password..."
         {...register("confirmPassword")}
       />
+      <p>{errors.confirmPassword?.message}</p>
       <br />
       <button type="sumbit">sumbit</button>
     </form>
